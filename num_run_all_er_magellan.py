a1ae9f52154b64d8f26d8a031e73ebdbaf58ce64
Lydia Chau <lydia.chau@mail.utoronto.ca>
2022-01-03 00:26:04 -0500

    add test code

---
+++ num_run_all_er_magellan.py
@@ -0,0 +1,72 @@
+import os
+import time
+
+datasets = """Dirty/DBLP-GoogleScholar
+Dirty/iTunes-Amazon
+Dirty/Walmart-Amazon
+Structured/Amazon-Google
+Structured/Beer
+Structured/Walmart-Amazon
+Textual/Abt-Buy
+Textual/Company""".split('\n')
+
+special_datasets = {
+    'Structured/Beer': (40, 8),
+    'Structured/iTunes-Amazon': (12, 8),
+    'Dirty/iTunes-Amazon': (12, 8),
+    'Textual/Company': (32, 8)
+}
+
+ops = """swap
+swap
+append_col
+del
+swap
+drop_col
+swap
+swap
+append_col
+drop_col
+drop_col
+swap
+del""".split('\n')
+
+
+lms = ['bert', 'bert', 'bert', 'bert', 'bert', 'bert',
+       'bert', 'bert']
+
+# lms = ['xlnet', 'roberta', 'roberta', 'roberta', 'xlnet', 'bert',
+#        'bert', 'xlnet', 'roberta', 'bert', 'roberta', 'roberta', 'bert']
+
+# lms = """distilbert
+# bert
+# xlnet
+# roberta""".split('\n')
+
+for dataset, op, lm in zip(datasets, ops, lms):
+    if dataset in special_datasets:
+        batch_size, epochs = special_datasets[dataset]
+    else:
+        batch_size, epochs = 12, 15
+
+    for da in [True, False]:
+        for dk in [True, False]:
+            for run_id in range(5):
+                cmd = """python train_ditto.py \
+              --task %s \
+              --logdir results_ditto/ \
+              --finetuning \
+              --batch_size %d \
+              --lr 3e-5 \
+              --fp16 \
+              --lm %s \
+              --n_epochs %d \
+              --run_id %d""" % (dataset, batch_size, lm, epochs, run_id)
+                if 'Company' in dataset:
+                    cmd += ' --summarize'
+                if da:
+                    cmd += ' --da %s' % op
+                if dk:
+                    cmd += ' --dk general'
+                print(cmd)
+                os.system(cmd)
